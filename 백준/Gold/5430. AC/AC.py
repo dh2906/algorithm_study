2ba2b97d3ec49dc6fd9ba495332de0a9830cea82
이동훈 <64298482+dh2906@users.noreply.github.com>
2025-10-23 09:33:31 +0900

    [Gold V] Title: AC, Time: 172 ms, Memory: 41512 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.py
@@ -0,0 +1,38 @@
+import sys
+from collections import deque
+
+t = int(sys.stdin.readline())
+
+for _ in range(t):
+    p = sys.stdin.readline()
+    n = int(sys.stdin.readline())
+    lst = deque(sys.stdin.readline()[1:-2].strip().split(','))
+
+    if n == 0:
+        lst = deque()
+
+    r_cnt = 0
+    flag = False
+
+    for op in p:
+        if op == 'R':
+            r_cnt += 1
+
+        elif op == 'D':
+            if lst:
+                if r_cnt % 2 == 0:
+                    lst.popleft()
+
+                else:
+                    lst.pop()
+
+            else:
+                print('error')
+                flag = True
+                break
+
+    if r_cnt % 2 != 0:
+        lst.reverse()
+
+    if not flag:
+        print('[' + ','.join(lst) + ']')
