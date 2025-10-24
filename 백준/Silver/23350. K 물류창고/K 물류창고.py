3a9483a72f3a52925b7bf8d39b74958c09a3eedd
이동훈 <64298482+dh2906@users.noreply.github.com>
2025-10-24 17:34:14 +0900

    [Silver I] Title: K 물류창고, Time: 60 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/23350. K 물류창고/K 물류창고.py
+++ 백준/Silver/23350. K 물류창고/K 물류창고.py
@@ -18,11 +18,11 @@ for _ in range(n):
 while dq:
     priority, weight = dq[0]
 
-    if current_priority == priority: # 가장 앞에 있는 컨테이너 우선순위가 낮은 경우 (먼저 적재해야 하는 경우)
-        if containers[priority] and containers[priority][-1] < weight: # 이미 적재된 상태에서 새로 적재하려는 중량이 더 무거운 경우 -> 재배치
+    if current_priority == priority:
+        if containers[priority] and containers[priority][-1] < weight:
             temp = []
 
-            while containers[priority] and containers[priority][-1] < weight: # 가벼운 컨테이너를 만나거나 빌 때까지 반복
+            while containers[priority] and containers[priority][-1] < weight:
                 res += containers[priority][-1]
                 temp.append(containers[priority].pop())
 
@@ -30,7 +30,7 @@ while dq:
             containers[priority].append(dq.popleft()[1])
             containers[priority] += temp[::-1]
 
-        else: # 처음 적재하거나 적재하려는 중량이 더 가벼운 경우 -> 적재 가능
+        else:
             res += weight
             containers[priority].append(dq.popleft()[1])
 
