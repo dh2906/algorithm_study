bb4c473aa3e5c0e0079632721b102f0f67487214
이동훈 <64298482+dh2906@users.noreply.github.com>
2025-11-11 19:59:57 +0900

    [Silver III] Title: N과 M (5), Time: 188 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,21 @@
+import sys
+
+def backtracking():
+    if len(arr) == m:
+        print(*arr)
+        return
+
+    for item in values:
+        if item not in arr:
+            arr.append(item)
+            backtracking()
+            arr.pop()
+
+n, m = map(int, sys.stdin.readline().split())
+values = list(map(int, sys.stdin.readline().split()))
+
+values.sort()
+
+arr = []
+
+backtracking()
